595f6697c839b488d01f5614623d4c72ed6176e7
Ayman Ali <ayman.ali1230@gmail.com>
2016-09-30 10:59:22 -0400

    minor change as result of adding results to repo

--- EoE_Optimization_Model/results.cpp
+++ EoE_Optimization_Model/results.cpp
@@ -15,7 +15,7 @@ Strategy_Results::Strategy_Results()
 void Strategy_Results::print_results(std::string const &strategy_tag)
 {
 	std::ofstream file; 
-	file.open("../../Results/" + strategy_tag + ".csv");
+	file.open("../Results/" + strategy_tag + ".csv");
 	/*header*/
 	file <<
 		"total patients," <<
